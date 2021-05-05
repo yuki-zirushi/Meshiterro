4e9293669a115938123c71b0e23a307d57c8cdb2
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-05 23:21:35 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '97adf4fa7c608ce8aeef52e47376f66f715370da8557bd6fb0574f6bbea2ed70ef02893a1225b3b07b5937a88ccaa1a8ad5055c31d71c778d90b9fe41057c292'
